58116511f94d9906fc8ac8a9fb9835d45197837c
Tejaswaroop Vankadari <tvankada@cisco.com>
2017-09-28 16:32:41 -0700

    Added Tessel code

--- constants.js
+++ constants.js
@@ -2,5 +2,5 @@
  * Created by tvankada on 9/27/17.
  */
 module.exports = {
-    SERVER_URL: 'https://roomfinder.cisco.com:8443/roomstatus?roomemail=CONF_87356@cisco.com',
+    SERVER_URL: 'https://172.29.184.143:8443/roomstatus?roomemail=CONF_87356@cisco.com',
 }
--- index.js
+++ index.js
@@ -4,11 +4,8 @@
 var request = require('request');
 var schedule = require('node-schedule');
 var constants = require('./constants');
+var tessel = require('tessel');
 
-
-var sys = require('sys')
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 console.log(constants.SERVER_URL);
 function execute() {
@@ -25,15 +22,19 @@ function execute() {
         roomStatus = JSON.parse(roomStatus);
         if (roomStatus.status === 'Busy') {
             console.log(roomStatus.status);
-            exec("t2 run /Users/tvankada/tessel-code/index.js", puts);
+            tessel.led[2].on();
+            tessel.led[3].on();
+           
         } else  if (roomStatus.status === 'Free') {
             console.log(roomStatus.status);
-            exec("t2 run /Users/tvankada/tessel-code/index2.js", puts);
+            tessel.led[2].off();
+            tessel.led[3].off();
+         
         }
     });
 }
 execute();
 // Code to run the execute function every 1 minute
-var j = schedule.scheduleJob('/10 * * * * *', function(){
+var j = schedule.scheduleJob('*/2 * * * * *', function(){
     execute();
 });
